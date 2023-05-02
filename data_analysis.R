fd56bdf32743317729659805401ed5ea58985ba7
Ciaran Evans <lughnasace@gmail.com>
2023-05-02 08:09:40 -0400

    editing code for paper revision

--- data_analysis.R
+++ data_analysis.R
@@ -1,11 +1,29 @@
 library(tidyverse)
 library(patchwork)
+library(effectsize)
+library(emmeans)
+library(xtable)
+
+# helper function for un-nesting data frames
+unnest_dataframes <- function(x) {
+  
+  y <- do.call(data.frame, x)
+  
+  if("data.frame" %in% sapply(y, class)) unnest_dataframes(y)
+  
+  y
+  
+}
+
+# import the cleaned data
 
 dog_data <- read_csv("dog_data_cleaned.csv")
 
 
-# First, check that we get the same results as Table 2
-# in the original paper
+# To verify that the variables were correctly created, summarize the 
+# mean of each variable for the pre/post tests in each treatment group
+# This summary does not appear in the teaching paper, but does reproduce 
+# Table 2 from Binfet et al.
 
 dog_data %>%
   mutate(GroupAssignment = factor(GroupAssignment, 
@@ -26,9 +44,11 @@ dog_data %>%
   View()
 
 
+### Section 4.2
 
-# Next, make some plots to visualize how students feel at the beginning of the
-# study. Are they generally happy, or stressed, etc.?
+# Figure 1: Pre-test distributions of well-being and ill-being measurements
+# (Make some plots to visualize how students feel at the beginning of the
+# study. Are they generally happy, or stressed, etc.?)
 
 p1 <- dog_data %>%
   filter(Stage == "pre") %>%
@@ -112,15 +132,52 @@ p9 <- dog_data %>%
   ylim(0,130)
 
      
-            
-
 pdf(file = "pre_test_scales.pdf", width = 7.5, height = 6)
 (p1+p2+p3)/(p4+p5+p6)/(p7+p8+p9)
 dev.off()
 
 
 
-# now look at differences pre- and post-test
+# Figure 2: Relationships between several well-being and ill-being measures
+
+p1 <- dog_data %>%
+  mutate(Group = factor(GroupAssignment, 
+                        levels = c("Control", "Indirect", "Direct"))) %>%
+  ggplot(aes(x = PANAS_PA, y = PANAS_NA, color = Group, shape = Group)) +
+  geom_point() +
+  geom_jitter(width = 0.1, height = 0.1) +
+  theme_bw() +
+  labs(x = "Positive affect score", y = "Negative affect score")
+
+
+p2 <- dog_data %>%
+  mutate(Group = factor(GroupAssignment, 
+                        levels = c("Control", "Indirect", "Direct"))) %>%
+  ggplot(aes(x = SCS, y = FS, color = Group, shape = Group)) +
+  geom_point() +
+  geom_jitter(width = 0.1, height = 0.1) +
+  theme_bw() +
+  labs(x = "Social connectedness", y = "Flourishing")
+
+
+p3 <- dog_data %>%
+  mutate(Group = factor(GroupAssignment, 
+                        levels = c("Control", "Indirect", "Direct"))) %>%
+  ggplot(aes(x = SCS, y = Lonely, color = Group, shape = Group)) +
+  geom_point() +
+  geom_jitter(width = 0.1, height = 0.1) +
+  theme_bw() +
+  labs(x = "Social connectedness", y = "Loneliness")
+
+pdf(file = "pairwise_relationships.pdf", width = 7.5, height = 4)
+comb.plot<-p1+p2+p3 & theme(legend.position = "bottom")
+comb.plot+plot_layout(guides = "collect")
+dev.off()
+
+
+
+# Figure 3: Differences between post-test and pre-test scores for several 
+# well-being and ill-being measures
 
 p1 <- dog_data %>%
   filter(Stage == "post") %>%
@@ -254,51 +311,11 @@ pdf(file = "pre_post_diff.pdf", width = 7.5, height = 6)
 (p1+p2+p3)/(p4+p5+p6)/(p7+p8+p9)
 dev.off()
 
-p1 <- dog_data %>%
-  mutate(Group = factor(GroupAssignment, 
-                        levels = c("Control", "Indirect", "Direct"))) %>%
-  ggplot(aes(x = PANAS_PA, y = PANAS_NA, color = Group, shape = Group)) +
-  geom_point() +
-  geom_jitter(width = 0.1, height = 0.1) +
-  theme_bw() +
-  labs(x = "Positive affect score", y = "Negative affect score")
 
+### 5.1: Hypothesis 1
 
-p2 <- dog_data %>%
-  mutate(Group = factor(GroupAssignment, 
-                        levels = c("Control", "Indirect", "Direct"))) %>%
-  ggplot(aes(x = SCS, y = FS, color = Group, shape = Group)) +
-  geom_point() +
-  geom_jitter(width = 0.1, height = 0.1) +
-  theme_bw() +
-  labs(x = "Social connectedness", y = "Flourishing")
-
-
-p3 <- dog_data %>%
-  mutate(Group = factor(GroupAssignment, 
-                        levels = c("Control", "Indirect", "Direct"))) %>%
-  ggplot(aes(x = SCS, y = Lonely, color = Group, shape = Group)) +
-  geom_point() +
-  geom_jitter(width = 0.1, height = 0.1) +
-  theme_bw() +
-  labs(x = "Social connectedness", y = "Loneliness")
-
-library(patchwork)
-pdf(file = "pairwise_relationships.pdf", width = 7.5, height = 4)
-comb.plot<-p1+p2+p3 & theme(legend.position = "bottom")
-comb.plot+plot_layout(guides = "collect")
-dev.off()
-
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-# Table 3
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-library(effectsize)
+# Table 1: Paired sample t-test statistics comparing well-being and ill-being 
+# measures before and after treatment
 
 dog_data %>%
   mutate(GroupAssignment = factor(GroupAssignment, 
@@ -312,72 +329,32 @@ dog_data %>%
                                       "Homesick", "Lonely", "PANAS_NA"),
                            labels = c("FS", "PANAS_PA", "SCS", "SHS", "Engagement", "Stress",
                                       "Homesick", "Lonely", "PANAS_NA"))) %>%
-  mutate(alt = case_when(variable %in% c("FS", "PANAS_PA", "SCS", "SHS", "Engagement") ~ "greater",
-                         variable %in% c("Stress","Homesick", "Lonely", "PANAS_NA") ~ "less"))%>%
   pivot_wider(values_from = value, names_from = Stage) %>%
   group_by(variable, GroupAssignment)%>%
   filter(complete.cases(pre,post)) %>%
   summarize(
-    t = round(t.test(post, pre, paired=T, alternative=unique(alt))$statistic, 2),
-    df = round(t.test(post, pre, paired=T, alternative=unique(alt))$parameter,2),
-    p = round(t.test(post, pre, paired=T, alternative=unique(alt))$p.value,3),
-    d = round(effectsize::cohens_d(post, pre, paired=T, alternative=unique(alt))$Cohens_d,2),
+    t = round(t.test(post, pre, paired=T, alternative="two.sided")$statistic, 2),
+    df = round(t.test(post, pre, paired=T, alternative="two.sided")$parameter,2),
+    p = round(t.test(post, pre, paired=T, alternative="two.sided")$p.value,3),
+    d = round(effectsize::cohens_d(post, pre, paired=T, alternative="two.sided")$Cohens_d,2),
     LL = round(effectsize::cohens_d(post, pre, paired=T, conf.level=0.95)$CI_low,2),
     UL = round(effectsize::cohens_d(post, pre, paired=T, conf.level=0.95)$CI_high,2)
   ) %>%
   arrange(GroupAssignment) %>%
   view()
 
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-# Table 4 (Hypothesis 2 -- Binfet et al contrast with a different approach -- Bonferonni Adjusted )
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-library(emmeans)
 
-dog_data %>%
-  mutate(GroupAssignment = factor(GroupAssignment, 
-                                  levels = c("Control", "Indirect", "Direct")))%>%
-  pivot_longer(cols = starts_with(c("FS", "PANAS_PA", "SCS", "SHS", "Engagement", "Stress",
-                                    "Homesick", "Lonely", "PANAS_NA")),
-               names_to = "variable",
-               values_to = "value")%>%
-  mutate(variable = factor(variable,
-                           levels = c("FS", "PANAS_PA", "SCS", "SHS", "Engagement", "Stress",
-                                      "Homesick", "Lonely", "PANAS_NA"),
-                           labels = c("FS", "PANAS_PA", "SCS", "SHS", "Engagement", "Stress",
-                                      "Homesick", "Lonely", "PANAS_NA"))) %>%
-  mutate(alt = case_when(variable %in% c("FS", "PANAS_PA", "SCS", "SHS", "Engagement") ~ "greater",
-                         variable %in% c("Stress","Homesick", "Lonely", "PANAS_NA") ~ "less"))%>%
-  pivot_wider(values_from = value, names_from = Stage) %>%
-  filter(complete.cases(variable,GroupAssignment)) %>%
-  group_by(variable)%>%
-  summarize(
-    beta = data.frame(contrast(emmeans(aov(post~pre+GroupAssignment), ~GroupAssignment),
-                               list("(Direct/Indirect)-Control" = c(-1, 0.5, 0.5),
-                                    "Direct - Indirect" = c(0, -1, 1)), adjust="bonferroni")),
-    effs = data.frame(eff_size(emmeans(aov(post~pre+GroupAssignment), ~GroupAssignment),
-                               method=list("(Direct/Indirect)-Control" = c(-1, 0.5, 0.5),
-                                           "Direct - Indirect" = c(0, -1, 1)),
-                               sigma=sigma(aov(post~pre+GroupAssignment)),
-                               edf=aov(post~pre+GroupAssignment)$df.residual))
-  ) %>%
-  view()
+### 5.2 and 5.3: Hypotheses 2 and 3
 
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-# Table 4 (Hypothesis 2 -- 'fixed' contrasts a statistician would run -- Bonferroni Adjusted )
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-library(emmeans)
+## Table 2: ANOVA contrast results comparing changes in well-being and ill-being 
+## measures across treatment groups
+
+# We want to test whether there are differences between the treatment groups
+# Hypothesis 2 from Binfet et al. can be tested by comparing the control group
+# with the other two treatments
+# Here, test the Indirect-Control and Direct-Control contrasts (Section 5.2, 
+# equation 4 in our teaching paper), and use a Bonferroni adjustment since we are 
+# doing two tests per variable
 tab1<-dog_data %>%
   mutate(GroupAssignment = factor(GroupAssignment, 
                                   levels = c("Control", "Indirect", "Direct")))%>%
@@ -404,18 +381,13 @@ tab1<-dog_data %>%
                                            "Indirect-Control" = c(-1, 1, 0)),
                                sigma=sigma(aov(post~pre+GroupAssignment)),
                                edf=aov(post~pre+GroupAssignment)$df.residual))
-  ) %>%
-  view()
+  )
 
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-# Table 4 (Hypothesis 4 -- Binfet et al contrast with a different approach -- Not Adjusted b/c it's one contrast per response)
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
+
+# Next, we want to test hypothesis 3 from Binfet et al. (Section 5.3, equation 8
+# in the teaching paper)
+# No Bonferroni adjustment is needed because we are only doing one test
+
 tab2<-dog_data %>%
   mutate(GroupAssignment = factor(GroupAssignment, 
                                   levels = c("Control", "Indirect", "Direct")))%>%
@@ -440,45 +412,13 @@ tab2<-dog_data %>%
                                method=list("Direct - Indirect" = c(0, -1, 1)),
                                sigma=sigma(aov(post~pre+GroupAssignment)),
                                edf=aov(post~pre+GroupAssignment)$df.residual))
-  ) %>%
-  view()
+  )
 
-unnest_dataframes <- function(x) {
-  
-  y <- do.call(data.frame, x)
-  
-  if("data.frame" %in% sapply(y, class)) unnest_dataframes(y)
-  
-  y
-  
-}
 
-tab12<-rbind(data.frame(tab1),data.frame(tab2))
-new_data <- unnest_dataframes(tab12)
-new_data<- new_data %>% 
-  select(variable,
-         beta.contrast,beta.estimate, beta.SE, beta.t.ratio, beta.p.value,
-         effs.effect.size, effs.lower.CL, effs.upper.CL) %>%
-  mutate(beta.contrast=factor(beta.contrast, 
-                              levels=c("Indirect-Control","Direct-Control", "Direct - Indirect"))) %>%
-  arrange(beta.contrast) %>%
-  select(-beta.contrast)
+# Now, instead of testing hypotheses 2 and 3 separately, what if we 
+# simply tested all pairwise comparisons for the three treatments, and 
+# adjusted with Tukey's HSD? (Section 5.3)
 
-colnames(new_data)<-c("Measure", "$beta$", "$SE$", "$t$", "$p$",
-                      "$d$", "lower", "upper")
-library(xtable)
-print(xtable(new_data), include.rownames=FALSE)
-view(new_data)
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-# Table 4 (Hypothesis 2-3 simultaneously -- Tukey adjusted all pairwise comparisons)
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-library(emmeans)
 tab3<-dog_data %>%
   mutate(GroupAssignment = factor(GroupAssignment, 
                                   levels = c("Direct","Indirect","Control")))%>%
@@ -499,14 +439,13 @@ tab3<-dog_data %>%
   summarize(
     beta = data.frame(pairs(emmeans(aov(post~pre+GroupAssignment), ~GroupAssignment))),
     effs = data.frame(eff_size(emmeans(aov(post~pre+GroupAssignment), ~GroupAssignment),
-                              sigma=sigma(aov(post~pre+GroupAssignment)),
-                              edf=aov(post~pre+GroupAssignment)$df.residual))
-  ) %>%
-  view()
+                               sigma=sigma(aov(post~pre+GroupAssignment)),
+                               edf=aov(post~pre+GroupAssignment)$df.residual))
+  )
 
 
-new_data <- unnest_dataframes(tab3)
-new_data<- new_data %>% 
+tab3 <- unnest_dataframes(tab3)
+tab3 <- tab3 %>% 
   select(variable,
          beta.contrast,beta.estimate, beta.SE, beta.t.ratio, beta.p.value,
          effs.effect.size, effs.lower.CL, effs.upper.CL) %>%
@@ -515,38 +454,28 @@ new_data<- new_data %>%
   arrange(beta.contrast) %>%
   select(-beta.contrast)
 
-colnames(new_data)<-c("Measure", "$beta$", "$SE$", "$t$", "$p$",
+colnames(tab3)<-c("Measure", "$beta$", "$SE$", "$t$", "$p$",
                       "$d$", "lower", "upper")
-library(xtable)
-print(xtable(new_data), include.rownames=FALSE)
-
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-# Test on what they do?
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-#################################################################################################################
-test.data <- dog_data %>%
-  mutate(GroupAssignment = factor(GroupAssignment, 
-                                  levels = c("Control", "Indirect", "Direct")))%>%
-  pivot_longer(cols = starts_with(c("FS", "PANAS_PA", "SCS", "SHS", "Engagement", "Stress",
-                                    "Homesick", "Lonely", "PANAS_NA")),
-               names_to = "variable",
-               values_to = "value")%>%
-  mutate(variable = factor(variable,
-                           levels = c("FS", "PANAS_PA", "SCS", "SHS", "Engagement", "Stress",
-                                      "Homesick", "Lonely", "PANAS_NA"),
-                           labels = c("FS", "PANAS_PA", "SCS", "SHS", "Engagement", "Stress",
-                                      "Homesick", "Lonely", "PANAS_NA"))) %>%
-  mutate(alt = case_when(variable %in% c("FS", "PANAS_PA", "SCS", "SHS", "Engagement") ~ "greater",
-                         variable %in% c("Stress","Homesick", "Lonely", "PANAS_NA") ~ "less"))%>%
-  pivot_wider(values_from = value, names_from = Stage) %>%
-  filter(complete.cases(variable,GroupAssignment))
+tukey_pvals <- tab3$`$p$`
 
 
-aov.obj <- aov(post~pre+GroupAssignment, data=test.data)
-data.frame(lmtest::coeftest(aov.obj, vcov. = sandwich::vcovHC(aov.obj))[,])
+# Finally, combine the results from hypotheses 2 and 3 into Table 2
+
+tab12<-rbind(data.frame(tab1),data.frame(tab2))
+new_data <- unnest_dataframes(tab12)
+new_data<- new_data %>% 
+  select(variable,
+         beta.contrast,beta.estimate, beta.SE, beta.t.ratio, beta.p.value,
+         effs.effect.size, effs.lower.CL, effs.upper.CL) %>%
+  mutate(beta.contrast=factor(beta.contrast, 
+                              levels=c("Indirect-Control","Direct-Control", "Direct - Indirect"))) %>%
+  arrange(beta.contrast) %>%
+  select(-beta.contrast)
+
+colnames(new_data)<-c("Measure", "Estimate", "$SE$", "$t$", "$p$ (Bonf.)",
+                      "$d$", "lower", "upper")
+
+new_data$`$p$ (Tukey)` <- tukey_pvals
+
+view(new_data)
 
--- dog_data_cleaning.R
+++ dog_data_cleaning.R
@@ -1,19 +1,18 @@
 library(tidyverse)
 
-dog_data <- read_csv("2020-05-05_canine_contact_data_raw.csv", 
-                     na=c("N/A", "", "x", "n/a", "Doesn't apply", "X", "9999"))
+# Import the raw data
+# A description of each variable in dog_data_raw.csv can be
+# found in the data dictionary (data_dictionary.pdf)
 
-dog_data <- dog_data %>%
-  select(-c(Gender, Age_Mo, Ethnicity, Ethnicity_Specify,
-            Hometown_city, Hometown_country, Local,
-            Live_with, Describe_pets,
-            Helpful, Connection_feel, Eye_contact,
-            Physical_proximity, Physically_touched))
+dog_data <- read_csv("dog_data_raw.csv")
 
-write_csv(dog_data, "dog_data_raw.csv")
+# Section 3.4 (data description)
+# dimensions of the raw data
+# 284 rows, 210 columns
+dim(dog_data)
 
 
-# Processing the data
+# Section 4.1: Processing the data
 
 # there are a few values in PN, L2, and SC which are outside the allowed range
 # replace these values with NAs 
@@ -22,20 +21,25 @@ write_csv(dog_data, "dog_data_raw.csv")
 dog_data <- dog_data %>%
   mutate(across(starts_with("PN"), ~ replace(.x, .x > 5, NA)),
          across(starts_with("L2"), ~ replace(.x, .x > 4, NA)),
-         across(starts_with("SC"), ~ replace(.x, .x > 6, NA))) %>%
-  mutate_at(paste("L1_", c(1, 5, 6, 9, 10, 15, 16, 19, 20), sep=""),
-            function(x){5-x}) %>%
-  mutate_at(paste("L2_", c(1, 5, 6, 9, 10, 15, 16, 19, 20), sep=""),
-            function(x){5-x}) %>%
-  mutate_at(paste("SC1_", c(3, 6, 7, 9, 11, 13, 15, 17, 18, 20), sep=""),
-            function(x){7-x}) %>%
-  mutate_at(paste("SC2_", c(3, 6, 7, 9, 11, 13, 15, 17, 18, 20), sep=""),
-            function(x){7-x}) %>%
-  mutate(Engagement1 = 6 - HO1_2,
+         across(starts_with("SC"), ~ replace(.x, .x > 6, NA)),
+         across(paste("L1_", c(1, 5, 6, 9, 10, 15, 16, 19, 20), sep=""),
+                function(x){5-x}),
+         across(paste("L2_", c(1, 5, 6, 9, 10, 15, 16, 19, 20), sep=""),
+                function(x){5-x}),
+         across(paste("SC1_", c(3, 6, 7, 9, 11, 13, 15, 17, 18, 20), sep=""),
+                function(x){7-x}),
+         across(paste("SC2_", c(3, 6, 7, 9, 11, 13, 15, 17, 18, 20), sep=""),
+                function(x){7-x}),
+         Engagement1 = 6 - HO1_2,
          Engagement2 = 6 - HO2_2)
 
 
-# now average across mulitple items to get scores
+# Section 4.1: calculating scores
+# The original data contains a column for each item on 
+# the survey. To get scores like the positive affect score, we need to 
+# average all items for that score. Here we average the scores, then
+# remove the original columns for the individual items.
+# The resulting data still contains 284 rows, but now only has 24 columns
 
 dog_data <- dog_data %>%
   mutate(PANAS_PA1 = rowMeans(select(dog_data,
@@ -70,6 +74,8 @@ dog_data <- dog_data %>%
                 Homesick1, Homesick2, Lonely1, 
                 Lonely2, PANAS_NA1, PANAS_NA2)
 
+dim(dog_data)
+
 
 # finally, let's make the data long not wide, by making a new column 
 # for stage (pre/post)
@@ -79,8 +85,10 @@ dog_data <- dog_data %>%
     cols = -c(RID, GroupAssignment, Age_Yrs, Year_of_Study,
               Live_Pets, Consumer_BARK),
     names_to = c(".value", "Stage"),
-    names_pattern = "([\\D]+)(\\d+)"
-  ) %>%
+    names_pattern = "([\\D]+)(\\d+)") %>%
   mutate(Stage = ifelse(Stage == 1, "pre", "post"))
+
+
+# export cleaned data
 
 write_csv(dog_data, "dog_data_cleaned.csv")
